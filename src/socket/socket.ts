cd740941a678456fe38cb09a25ef2cfc012ce9f3
Rodolfo Rodriguez <rodolfo2smr@gmail.com>
2022-07-25 02:13:33 +0200

    fix: creacion de multiples chat sbre el mismo anuncio

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,8 @@
 import { Server, Socket } from "socket.io";
 import { Chat } from "../models/Chat";
 import { Message, IMessage } from "../models/Message";
+import { IUser } from "../models/User";
+import { IAdvertisement } from "../models/Advertisement";
 
 class SocketService {
   private io: Server;
@@ -47,6 +49,8 @@ class SocketService {
             creationDate: new Date().toISOString(),
             read: false,
           });
+          let newChat;
+          let newPopulatedChat;
           //2-  comprobar si existe chat, si me pasan el id existe, si no, no
           //si existe añado el mensaje, si no existe, creo el chat y añado el primer mensaje
           if (data.chatId) {
@@ -54,17 +58,31 @@ class SocketService {
               $addToSet: { messages: [message._id] },
             });
           } else {
-            await Chat.create({
+            newChat = await Chat.create({
               advertId: data.advertId,
               members: [data.message.sender, data.message.receiver],
               messages: [message._id],
             });
+
+            newPopulatedChat = (await Chat.findOne({
+              _id: newChat?.["_doc"]?._id,
+            })
+              .populate<{ members: Array<IUser> }>("members")
+              .populate<{ messages: Array<IMessage> }>("messages")
+              .populate<{ advertId: IAdvertisement }>("advertId")
+              .lean()) as any;
           }
           //3- Emito el contenido del mensaje para que el front actualice aitomaticamente el mensaje en directo
           // socket.to(sender).emit("getPrivateMessage", data.message);
-          socket.to(receiver).emit("getPrivateMessage", {
-            chatId: data.chatId,
+          socket.emit("privateMessageSent", {
+            chatId: data.chatId || newPopulatedChat?._id,
             message: message["_doc"],
+            newChat: newPopulatedChat || null,
+          });
+          socket.to(receiver).emit("getPrivateMessage", {
+            chatId: data.chatId || newPopulatedChat?._id,
+            message: message["_doc"],
+            newChat: newPopulatedChat || null,
           });
         }
       );
@@ -77,20 +95,20 @@ class SocketService {
           // message: { content: string; sender: string; receiver: string };
         }) => {
           if (data.chatId) {
-          const chat = await Chat.findById(data.chatId)
-            .populate<{ messages: Array<IMessage> }>("messages")
-            .orFail()
-            .lean();
-          const unreadMessagesIds = chat?.messages
-            ?.filter(
-              (c) => c.receiver?.toString() === data?.receiverId && !c.read
-            )
-            ?.map((x) => x._id.toString());
-          await Message.updateMany(
-            { _id: { $in: unreadMessagesIds } },
-            { $set: { read: true } },
-            { multi: true }
-          );
+            const chat = await Chat.findById(data.chatId)
+              .populate<{ messages: Array<IMessage> }>("messages")
+              .orFail()
+              .lean();
+            const unreadMessagesIds = chat?.messages
+              ?.filter(
+                (c) => c.receiver?.toString() === data?.receiverId && !c.read
+              )
+              ?.map((x) => x._id.toString());
+            await Message.updateMany(
+              { _id: { $in: unreadMessagesIds } },
+              { $set: { read: true } },
+              { multi: true }
+            );
           }
         }
       );
